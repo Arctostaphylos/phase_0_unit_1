9556717011961bd2571ffc89a1bcf61d514ccafc
Arctostaphylos <shanks@sonic.net>
2014-04-14 10:49:45 -0700

    Update my_solution.js

--- week_3/1_code_combat/my_solution.js
+++ week_3/1_code_combat/my_solution.js
@@ -4,9 +4,35 @@
 // Note: to make commenting easier, you can highlight the section you want to comment and hold 
 // command + / This will comment the line. 
  
+// move up
+// move right x2
+// move down
+
+this.moveDown();
+this.moveDown();
+this.moveRight();
+
+this.moveUp();
+this.moveUp();
+this.moveRight();
+this.moveRight();
+this.moveDown();
+this.attackNearbyEnemy();
+
+//Grab the mushroom
+// move right & pick up mushroom
+// move left
+// move up
+// attack
  
- 
- 
+ this.moveUp();
+this.moveRight();
+this.moveLeft();
+this.moveUp();
+
+this.attackNearbyEnemy();
+
+
  
  
  
