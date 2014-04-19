71d7448cd4d8021d49ccda52fba3070ca6f090f5
Arctostaphylos <shanks@sonic.net>
2014-04-19 01:28:39 -0700

    Update and rename my_solution.js to gc1_refactored.js

---
+++ week_3/5_group_project/gc1_refactored.js
@@ -0,0 +1,29 @@
+var total=0
+
+var sum=function(arr) {
+for(i=0; i<arr.length; i++) {
+total+=arr[i];
+console.log(i);
+}
+}
+
+//Test 2
+
+var mean=function(arr) {
+total=sum(arr);
+answer=total/arr.length;
+console.log(answer);
+}
+
+//Test 3
+
+var median=function(arr) {
+index.sort;
+if(arr[i]%2===1){
+answer=(arr.length/2).floor
+console.log(answer)
+} else {
+answer=((arr.length/2)+1).floor
+console.log(answer)
+}
+}
